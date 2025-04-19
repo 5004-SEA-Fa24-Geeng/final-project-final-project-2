d45d6531b39393b498bd5370ba783ce4436a87b9
Xingchen Liu <ivanliuxingchen@163.com>
2025-04-18 21:12:28 -0700

    update

--- src/main/java/model/IImporter.java
+++ src/main/java/model/IImporter.java
@@ -2,10 +2,27 @@ package model;
 
 import java.util.List;
 
+/**
+ * Interface defining operations for importing data.
+ * Provides methods for importing influencer data from different sources.
+ */
 public interface IImporter {
 
+    /**
+     * Imports influencer data from a file path.
+     *
+     * @param filePath the path to the file containing the data
+     * @return a list of imported Influencer objects
+     */
     List<Influencer> importData(String filePath);
 
+    /**
+     * Imports influencer data directly from a file.
+     * This method can be used for more specific file import operations.
+     *
+     * @param filePath the path to the file containing the data
+     * @return a list of imported Influencer objects
+     */
     List<Influencer> importFromFile(String filePath);
 
 }
